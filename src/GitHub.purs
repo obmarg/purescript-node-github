f8ad1b507e76045479ed6cc4aa380ee0ee5940dd
Graeme Coupar <grambo@grambo.me.uk>
2014-11-11 22:21:58 +0000

    Some general client construction code.
    - A constructor for clients.
    - A function that can be used to get functions from clients.
      This will form the basis of the entire library hopefully - just a
      bunch of parameters passed to these functions to construct the rest of
      the API.

--- src/GitHub.purs
+++ src/GitHub.purs
@@ -1,3 +1,24 @@
 module GitHub where
 
 import Debug.Trace
+import Node.Thunk
+
+foreign import data Client :: *
+
+foreign import makeClient """
+    function makeClient(opts){
+        var GitHubApi = require('github');
+
+        return GitHubApi(opts);
+    }
+""" :: forall a. { | a } -> Client
+
+type ClientFn = forall a b. ThunkFn1 { | a} { |b}
+
+foreign import getClientFn """
+    function getClientFn(propName) {
+    return function(fnName){
+    return function(client) {
+        return client[propName][fnName];
+    }}}
+""" :: String -> String -> Client -> ClientFn
