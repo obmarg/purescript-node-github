597f9dc5aa0197b8b3335539ba51d574003d0dae
Graeme Coupar <grambo@grambo.me.uk>
2014-11-12 00:04:34 +0000

    Some README & example work.

---
+++ example/example.purs
@@ -0,0 +1,30 @@
+module Main where
+
+import Debug.Trace
+import Data.Either
+import Control.Monad.Eff
+import Node.Thunk
+
+import GitHub
+import GitHub.User
+
+foreign import printInspect """
+    function printInspect(any) {
+        return function(){
+            var util = require('util');
+
+            console.log(util.inspect(any));
+
+            return {};
+        }
+    }
+""" :: forall a. a -> Eff (trace :: Debug.Trace.Trace) Unit
+
+getAndPrintFollowing = do
+    result <- getFollowingFromUser client { user: "obmarg" }
+    liftEff (printInspect result)
+      where client = makeClient {version: "3.0.0"}
+
+main = runThunk getAndPrintFollowing handle
+    where handle (Left err) = print err
+          handle (Right result) = print "Done"
