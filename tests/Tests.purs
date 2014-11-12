c547ae59e0b573c149b72f73c591e9ff8fb6b202
Graeme Coupar <grambo@grambo.me.uk>
2014-11-12 00:42:44 +0000

    Moved Main code into tests
    This will allow me to run "tests" during grunt, but not have those tests
    included in packages distributed by bower.

--- tests/Tests.purs
+++ tests/Tests.purs
@@ -1,12 +1,20 @@
 module Main where
 
+import Data.Array
+import Data.Maybe
+
 import Debug.Trace
 import Data.Either
 import Control.Monad.Eff
+
 import Node.Thunk
 
+-- Import the library's module(s)
 import GitHub
 import GitHub.User
+
+-- Import Test.QuickCheck, which supports property-based testing
+import Test.QuickCheck
 
 foreign import printInspect """
     function printInspect(any) {
@@ -28,3 +36,5 @@ getAndPrintFollowing = do
 main = runThunk getAndPrintFollowing handle
     where handle (Left err) = print err
           handle (Right result) = print "Done"
+
+-- TODO: Maybe write some _actual_ tests.
